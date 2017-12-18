bb59e7a4b7555b6b4b5f4e80219868f8f2bb965e
Francesco Ferrante <francesco.ferrante256@gmail.com>
2017-12-18 13:24:24 +0100

    bayesian modelselection added for monk1-2 and mlcup

--- src/main.m
+++ src/main.m
@@ -11,15 +11,24 @@ monk1_test_classes = monk1_test_set(:,1) * 2 - 1;
 monk1_test_features = monk1_test_set(:,2:end);
 
 % Model selection
-[monk1_kernel, monk1_nu] = svm_select_model(monk1_train_features, monk1_train_classes, 5, {0,2});
+monk1_kernels = containers.Map({'linear', 'gaussian'}, {0,2});
+
+[monk1_kernel_bayes, monk1_nu_bayes] = svm_select_model_bayesianly(monk1_train_features, monk1_train_classes, 5, monk1_kernels);
+[monk1_kernel, monk1_nu] = svm_select_model(monk1_train_features, monk1_train_classes, 5, {0,2}, 0.1:0.1:1);
+
+fprintf("bayesian search \t| grid search \n ker: %d, nu: %f \t| ker: %d nu: %f\n", monk1_kernel_bayes, monk1_nu_bayes, monk1_kernel, monk1_nu);
 
 % Training
-monk1_model = svm_train(monk1_train_features, monk1_train_classes, monk1_kernel , monk1_nu); 
+monk1_model = svm_train(monk1_train_features, monk1_train_classes, monk1_kernel , monk1_nu);
+monk1_model_bayes = svm_train(monk1_train_features, monk1_train_classes, monk1_kernel_bayes, monk1_nu_bayes);
 
 % Test
 monk1_predictions = svm_predict(monk1_model, monk1_test_features);
+monk1_predictions_bayes = svm_predict(monk1_model_bayes, monk1_test_features);
 monk1_model_accurancy = sum(monk1_test_classes == monk1_predictions) / length(monk1_test_classes)
+monk1_model_bayes_accurancy = sum(monk1_test_classes == monk1_predictions_bayes) / length(monk1_test_classes)
 
+pause
 
 %% Monk 2 dataset experiment
 
@@ -33,37 +42,48 @@ monk2_train_features = monk2_training_set(:,2:end);
 monk2_test_classes = monk2_test_set(:,1) * 2 - 1;
 monk2_test_features = monk2_test_set(:,2:end);
 
-% Model selection
-[monk2_kernel, monk2_nu] = svm_select_model(monk2_train_features, monk2_train_classes, 5, {0,2});
+% Model Selection
 
-% Training
-monk2_model = svm_train(monk2_train_features, monk2_train_classes, monk2_kernel , monk2_nu); 
+monk2_kernels = containers.Map({'linear', 'gaussian'}, {0,2});
+
+[monk2_kernel_bayes, monk2_nu_bayes] = svm_select_model_bayesianly(monk2_train_features, monk2_train_classes, 5, monk2_kernels);
+[monk2_kernel, monk2_nu] = svm_select_model(monk2_train_features, monk2_train_classes, 5, {0,2}, 0.1:0.1:1);
+
+fprintf("bayesian search \t| grid search \n ker: %d, nu: %f \t| ker: %d nu: %f\n", monk2_kernel_bayes, monk2_nu_bayes, monk2_kernel, monk2_nu);
+
+% Training 
+monk2_model = svm_train(monk2_train_features, monk2_train_classes, monk2_kernel, monk2_nu);
+monk2_model_bayes = svm_train(monk2_train_features, monk2_train_classes, monk2_kernel_bayes, monk2_nu_bayes);
 
 % Test
 monk2_predictions = svm_predict(monk2_model, monk2_test_features);
+monk2_predictions_bayes = svm_predict(monk2_model_bayes, monk2_test_features);
 monk2_model_accurancy = sum(monk2_test_classes == monk2_predictions) / length(monk2_test_classes)
+monk2_model_bayes_accurancy = sum(monk2_test_classes == monk2_predictions_bayes) / length(monk2_test_classes)
 
-%% Monk 3 dataset experiment
+pause
 
-% Data import and labels fixing
-monk3_training_set = importdata("../data/monks-3.train.csv");
-monk3_test_set = importdata("../data/monks-3.test.csv");
-
-monk3_train_classes = monk3_training_set(:,1) * 2 - 1;
-monk3_train_features = monk3_training_set(:,2:end);
-
-monk3_test_classes = monk3_test_set(:,1) * 2 - 1;
-monk3_test_features = monk3_test_set(:,2:end);
-
-% Model selection
-[monk2_kernel, monk2_nu] = svm_select_model(monk2_train_features, monk2_train_classes, 5, {0,2});
-
-% Training
-monk3_model = svm_train(monk3_train_features, monk3_train_classes, monk3_kernel , monk3_nu); 
-
-% Test
-monk3_predictions = svm_predict(monk3_model, monk3_test_features);
-monk3_model_accurancy = sum(monk3_test_classes == monk3_predictions) / length(monk3_test_classes)
+% %% Monk 3 dataset experiment
+% 
+% % Data import and labels fixing
+% monk3_training_set = importdata("../data/monks-3.train.csv");
+% monk3_test_set = importdata("../data/monks-3.test.csv");
+% 
+% monk3_train_classes = monk3_training_set(:,1) * 2 - 1;
+% monk3_train_features = monk3_training_set(:,2:end);
+% 
+% monk3_test_classes = monk3_test_set(:,1) * 2 - 1;
+% monk3_test_features = monk3_test_set(:,2:end);
+% 
+% % Model selection
+% [monk2_kernel, monk2_nu] = svm_select_model(monk2_train_features, monk2_train_classes, 5, {0,2}, 0.1:0.1:1);
+% 
+% % Training
+% monk3_model = svm_train(monk3_train_features, monk3_train_classes, monk3_kernel , monk3_nu); 
+% 
+% % Test
+% monk3_predictions = svm_predict(monk3_model, monk3_test_features);
+% monk3_model_accurancy = sum(monk3_test_classes == monk3_predictions) / length(monk3_test_classes)
 
 %% MLCUP17 dataset experiment
 
@@ -78,12 +98,23 @@ mlcup_train_outputs2 = mlcup_training_set(:,end);
 mlcup_test_ids = mlcup_test_set(:,1);
 mlcup_test_inputs = mlcup_test_set(:,2:end);
 
-
 % Model selection 
-[mlcup_kernel1, mlcup_nu1, mlcup_C1] = svr_select_model(mlcup_train_inputs, mlcup_train_outputs1, {0,2});
-[mlcup_kernel2, mlcup_nu2, mlcup_C2] = svr_select_model(mlcup_train_inputs, mlcup_train_outputs2, {0,2});
+mlcup_nus = [0.1:0.1:1];
+mlcup_Cs = logspace(-3,3,7);
+kernel_container = containers.Map({'linear', 'cubic', 'gaussian'}, {0,1,2});
+
+[mlcup_kernel1_bayes, mlcup_nu1_bayes, mlcup_C1_bayes] = svr_select_model_bayesianly(mlcup_train_inputs, mlcup_train_outputs1, 5, kernel_container);
+
+pause
+
+[mlcup_kernel1, mlcup_nu1, mlcup_C1, mean, devi] = svr_select_model(mlcup_train_inputs, mlcup_train_outputs1, 5, {1,2}, mlcup_nus, mlcup_Cs);
+
+fprintf("bayesian search \t| grid search \n ker: %d, nu: %f, C:%f \t| ker: %d nu: %f C:%f\n", monk2_kernel_bayes, monk2_nu_bayes, monk2_kernel, monk2_nu);
 
 
+stop
+[mlcup_kernel2, mlcup_nu2, mlcup_C2] = svr_select_model(mlcup_train_inputs, mlcup_train_outputs2, 5, {0,1,2});
+ 
 % Training models
 [mlcup_model1, mlcup_epsilon1] = svr_train(mlcup_train_inputs, mlcup_train_outputs1, mlcup_kernel1, mlcup_nu1, mlcup_C1);
 [mlcup_model2, mlcup_epsilon2] = svr_train(mlcup_train_inputs, mlcup_train_outputs2, mlcup_kernel2, mlcup_nu2, mlcup_C2);
