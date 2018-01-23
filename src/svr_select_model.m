c7fbd543a0ef48fc98d9f30bdbcacbe60a58666b
Francesco Ferrante <francesco.ferrante256@gmail.com>
2018-01-23 12:18:39 +0100

    Updated SVR model selection

--- src/svr_select_model.m
+++ src/svr_select_model.m
@@ -1,15 +1,18 @@
-function [kernel, C, epsilon, mean_validation_mse, devi_validation_mse] = svr_select_model(inputs, outputs, folds, kernels, C_range, epsilon_range)
+function [kernel, C, epsilon, mean_validation_mee, devi_validation_mee] = svr_select_model(inputs, outputs, folds, kernels, C_range, epsilon_range, algorithm, varargin)
 % SVR_SELECT_MODEL Selects the hyperparameters of a SVR via cross-validation
 %
-% SYNOPSIS: [kernel, C, epsilon] = svr_select_model(inputs, outputs, folds, kernels, C_range, epsilon_range)
+% SYNOPSIS: [kernel, C, epsilon] = svr_select_model(inputs, outputs, folds, kernels, C_range, epsilon_range, algorithm)
+%           [kernel, C, epsilon] = svr_select_model(inputs, outputs, folds, kernels, C_range, epsilon_range, 'bundleizator', precision)
 %
 % INPUT:
 % - inputs: a matrix containing one input sample per row
-% - outputs: a column vector containing one output sample per entry
+% - outputs: a matrix containing one output sample vector per row
 % - folds: the number of folds for the cross-validation
-% - kernels: a cell array of kernel functions
+% - kernels: a map of strings -> kernel functions
 % - C_range: a vector containing C values to try
 % - epsilon_range: a vector containing epsilon values to try
+% - algorithm: which implementation to use (bundleizator/libsvm)
+% - precision: the required distance from optimality (optional, only for bundleizator)
 %
 % OUTPUT:
 % - kernel: the best kernel function selected
@@ -20,68 +23,70 @@ function [kernel, C, epsilon, mean_validation_mse, devi_validation_mse] = svr_se
 %
 % SEE ALSO svm_select_model, svr_train, svr_predict
 
-    best_mse = +Inf;
-    dataset_partition = kfolds_partition(size(inputs, 1), folds);
+best_mee = +Inf;
+dataset_partition = kfolds_partition(size(inputs, 1), folds);
 
-    for kernel_index = 1:length(kernels)
-        % try different kernel functions
+for kernel_index = kernels.keys
+    % try different kernel functions
 
-        % graph of the MSE function
-        figure%(kernel_index);
-        %hold on;
-        grid on;
-        xlabel('C');
-        ylabel('\epsilon');
-        zlabel('MSE');
-        set(gca,'XScale','log');
+    % graph of the MEE function
+    figure;
+    grid on;
+    xlabel('C');
+    ylabel('\epsilon');
+    zlabel('MEE');
+    set(gca, 'XScale', 'log');
 
-        mean_validation_mse = zeros(length(C_range), length(epsilon_range));
-        devi_validation_mse= zeros(length(C_range), length(epsilon_range));
-        
-        for i = 1:length(C_range)
-            fprintf('\nC = %f, eps = ', C_range(i));
-            for j = 1:length(epsilon_range)
-                % try different values of the hyperparameters
-                fprintf('%f ', epsilon_range(j));
-                validation_mse = zeros(1, folds);  % keeps the validation MSE for each fold
+    mean_validation_mee = zeros(length(C_range), length(epsilon_range));
+    devi_validation_mee= zeros(length(C_range), length(epsilon_range));
+
+    % try different values of the hyperparameters
+    for i = 1:length(C_range)
+        fprintf('\nC = %f, eps = ', C_range(i));
+        for j = 1:length(epsilon_range)
+            fprintf('%f ', epsilon_range(j));
+            
+            % keeps the validation MEE for each fold
+            validation_mee = zeros(1, folds);
+
+            for fold_index = 1:folds
+                % pick one fold at a time for validation, leave the others for training
+                training_inputs = inputs((dataset_partition ~= fold_index),:);
+                training_outputs = outputs((dataset_partition ~= fold_index),:);
+                validation_inputs = inputs((dataset_partition == fold_index),:);
+                validation_outputs = outputs((dataset_partition == fold_index),:);
                 
-                failure = false;
-                
-                for fold_index = 1:folds
-                    % pick one fold at a time for validation, leave the others for training
-                    training_inputs = inputs((dataset_partition ~= fold_index),:);
-                    training_outputs = outputs((dataset_partition ~= fold_index),:);
-                    validation_inputs = inputs((dataset_partition == fold_index), :);
-                    validation_outputs = outputs((dataset_partition == fold_index),:);
-                    % train and estimate the validation MSE for this partition
-                    model = svr_train(training_inputs, training_outputs, kernels{kernel_index}, C_range(i), epsilon_range(j));
-                    
-                   failure = ~isstruct(model);
-                   if failure 
-                       break
-                   end
-                    predictions = svr_predict(model, validation_inputs);
-                    validation_mse(fold_index) = sum((predictions - validation_outputs) .^ 2) / length(validation_outputs);
+                % train the regression model for each output component and predict outputs
+                predictions = zeros(size(validation_outputs));
+                for k = 1:size(outputs, 2)
+                    if isempty(varargin)
+                        model = svr_train(training_inputs, training_outputs(:,k), kernels(kernel_index{1}), C_range(i), epsilon_range(j), algorithm);
+                    else
+                        model = svr_train(training_inputs, training_outputs(:,k), kernels(kernel_index{1}), C_range(i), epsilon_range(j), algorithm, varargin{1});
+                    end
+                    predictions(:,k) = svr_predict(model, validation_inputs, algorithm);
                 end
                 
-               if failure 
-                   continue
-               end
-                mean_validation_mse(i,j) = mean(validation_mse);
-                devi_validation_mse(i,j) = std(validation_mse);
+                % estimate the validation MEE for this partition
+                validation_mee(fold_index) = sum(sqrt(sum(((predictions - validation_outputs) .^ 2), 2))) / length(validation_outputs);
+            end
+            
+            mean_validation_mee(i,j) = mean(validation_mee);
+            devi_validation_mee(i,j) = std(validation_mee, 1);
 
-                surf(C_range, epsilon_range, mean_validation_mse', devi_validation_mse','FaceColor', 'interp');
-                drawnow
- 
-                if mean_validation_mse(i,j) < best_mse
-                    % we've found a new better model from these hyperparameter settings
-                    best_mse = mean_validation_mse(i,j);
-                    kernel = kernels{kernel_index};
-                    C = C_range(i);
-                    epsilon = epsilon_range(j);
-                end
+            surf(C_range, epsilon_range, mean_validation_mee', devi_validation_mee', 'FaceColor', 'interp');
+            set(gca, 'XScale', 'log');
+            drawnow;
+
+            if mean_validation_mee(i,j) < best_mee
+                % we've found a new better model from these hyperparameter settings
+                best_mee = mean_validation_mee(i,j);
+                kernel = kernels(kernel_index{1});
+                C = C_range(i);
+                epsilon = epsilon_range(j);
             end
         end
     end
+end
       
 end
