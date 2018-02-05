1693f0d30b306affe798e813f3717278567246a9
Francesco Ferrante <francesco.ferrante256@gmail.com>
2018-02-05 22:00:21 +0100

    Refactoring of bundleizator

--- src/bundleizator.m
+++ src/bundleizator.m
@@ -1,8 +1,8 @@
-function [u_star, iterations] = bundleizator(X, y, C, kernel, loss, dloss, precision, varargin)
+function [u, t, epsilon] = bundleizator(X, y, C, kernel, loss, dloss, precision, varargin)
 %BUNDLEIZATOR Implements a bundle method that solves a generic SVM
 %
-% SYNOPSIS: u_star = bundleizator(X, y, C, kernel, loss, dloss, precision)
-%           [u_star, iterations] = bundleizator(X, y, C, kernel, loss, dloss, precision)
+% SYNOPSIS: u = bundleizator(X, y, C, kernel, loss, dloss, precision)
+%           [u, t] = bundleizator(X, y, C, kernel, loss, dloss, precision)
 %           [...] = bundleizator(X, y, C, kernel, loss, dloss, precision, gram_svd_threshold)
 %
 % INPUT:
@@ -18,9 +18,9 @@ function [u_star, iterations] = bundleizator(X, y, C, kernel, loss, dloss, preci
 %         this threshold are discarded (optional, default 1e-6)
 %
 % OUTPUT:
-% - u_star: the optimal values for the coefficients of the linear
+% - u: the optimal values for the coefficients of the linear
 %           combination of support vectors
-% - iterations: the number of optimization loop iterations done
+% - t: the number of optimization loop iterations done
 %
 % SEE ALSO bundleizator_pruning, bundleizator_aggr
 
@@ -43,6 +43,7 @@ G = gram_matrix(X, kernel);
 % this is necessary for inverse operations since G is ill-conditioned
 [GU,GS,GV] = svd(G);
 % discard all singular values below threshold
+
 Gselector = diag(GS) >= gram_svd_threshold;
 sGS = GS(Gselector,Gselector);
 sGU = GU(:,Gselector);
@@ -50,15 +51,20 @@ sGV = GV(:,Gselector);
 
 %% Zero-th step
 t = 0;
-% we take a_0, b_0 = 0
+u = zeros(num_samples,1);
 
-% since the regularizer function is quadratic, its minimum is 0
-u_t = zeros(num_samples,1);
-J_t = 0;
+% Compute Remp at point u_0
+Remp = 0;
+f = zeros(num_samples,1);
+for i = 1:num_samples
+    Remp = Remp + loss(f(i), y(i));
+end
+Remp = Remp / num_samples;
 
-% no J_0(u_-1), so to make min(Jmin, J_1(u_0)) work...
-Jmin = Inf;
+% the quadratic term in u_0 is 0, so J(u_0) = Remp
+Jmin = Remp;
 
+% variables initialization
 A = [];
 b = [];
 H = [];
@@ -66,64 +72,62 @@ vdloss = zeros(num_samples, 1);
 
 %% Optimization loop
 while true
-    % Compute Remp and dloss at point u_t
-    Remp = 0;
-    f = G * u_t;
+    % Increment step
+    t = t +1;
+    
+    % Compute a_t
+    % compute dloss at point u_t-1
     for i = 1:num_samples
         vdloss(i) = dloss(f(i), y(i));
+    end
+    
+    % A(:,t) = G * vdloss / num_samples; 
+    A(:,t) = (sGU * (sGS * (sGV' * vdloss))) / num_samples;
+    
+    % Compute b_t
+    b(t,1) = Remp - A(:,t)' * u;
+    
+    % Update H
+    % h = (A(:,t)' / G) * A;
+    h = (((A(:,t)' * sGV) / sGS) * sGU') * A;
+    H = [H, h(1:t-1)'; h];
+    
+    % Solve the dual of the quadratic master problem
+    z = quadprog(0.5 * C * H, -b, -eye(t), zeros(t,1), ones(1,t), 1, [], [], [], quadprog_options);
+    % Get optimal point thru dual connection
+    % u_t = -0.5 * C * (G \ (A * z_t));
+    u = -0.5 * C * (sGV * (sGS \ (sGU' * (A * z))));
+
+    % Compute Remp at point u_t
+    Remp = 0;
+    % f = G * u;
+    f = (sGU * (sGS * (sGV' * u)));
+    for i = 1:num_samples
         Remp = Remp + loss(f(i), y(i));
     end
     Remp = Remp / num_samples;
     
-    % Compute a_t+1
-    A(:,t+1) = G * vdloss / num_samples;
+    % Compute J(u_t)
+    % J =  1/C * (u' * G * u) + Remp;
+    J =  1/C * (u' * (sGU * (sGS * (sGV' * u)))) + Remp;
     
-    % Compute b_t+1
-    b(t+1,1) = Remp - A(:,t+1)' * u_t;
-    
-    % Evaluate J_t+1 at point u_t
-    R_t1 = max(u_t' * A + b');
-    J_t1 = 1/C * (u_t' * G * u_t) + R_t1;
+    % Evaluate J_t at point u_t
+    R_t = max(u' * A + b');
+    % J_t = 1/C * (u' * G * u) + R_t;
+    J_t =  1/C * (u' * (sGU * (sGS * (sGV' * u)))) + R_t;
     
     % Compute epsilon
-    Jmin = min(Jmin, J_t1);
+    Jmin = min(Jmin, J);
     epsilon = Jmin - J_t;
     
     % Output iteration status
-    fprintf('t = %d\t Remp = %e\t J_t = %e\t J(u_t) = %e\t e_t = %e\n', t, Remp, J_t, J_t1, epsilon);
+    fprintf('t = %d\t Jmin = %e\t J_t(u_t) = %e\t e_t = %e\n', t, Jmin, J_t, epsilon);
     
     % Halt when we reach the desired precision
     if epsilon <= precision
         break
     end
     
-    % Update H
-    % h = (A(:,t+1)' / G) * A;
-    h = (((A(:,t+1)' * sGV) / sGS) * sGU') * A;
-    H = [H, h(1:t)'; h];
-    
-    % Increment step
-    t = t + 1;
-    
-    % Solve the dual of the quadratic master problem
-    z_t = quadprog(0.5 * C * H, -b, -eye(t), zeros(t,1), ones(1,t), 1, [], [], [], quadprog_options);
-    % Get optimal point thru dual connection
-    % u_t = -0.5 * C * (G \ (A * z_t));
-    u_t = -0.5 * C * (sGV * (sGS \ (sGU' * (A * z_t))));
-    
-    % Evaluate J_t at point u_t
-    R_t = max(u_t' * A + b');
-    J_t = 1/C * (u_t' * G * u_t) + R_t;
-end
-
-%% Function outputs
-
-% Optimal value of u
-u_star = u_t;
-
-% Number of iterations, if requested
-if nargout == 2
-    iterations = t;
 end
 
 end
