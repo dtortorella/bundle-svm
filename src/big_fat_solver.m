278abcbc4a194183aae19038108eb9ffd2e6e0c3
Domenico Tortorella <dom.tortorella@gmail.com>
2020-02-05 15:46:49 +0100

    Big fat solver

---
+++ src/big_fat_solver.m
@@ -0,0 +1,56 @@
+function [u, sv, J] = big_fat_solver(X, y, C, kernel, loss, varargin)
+%BIG_FAT_SOLVER Quadratic program solver for a SVM/SVR
+%
+% SYNOPSIS: [u, sv] = big_fat_solver(X, y, C, kernel, 'hinge')
+%           [u, sv] = big_fat_solver(X, y, C, kernel, 'epsilon', eps)
+%
+% INPUT:
+% - X: a matrix containing one sample feature vector per row
+% - y: a column vector containing one sample target per entry
+% - C: inverse of the regularization constant (1/lambda)
+% - kernel: a function that computes the scalar product of two vectors in feature space
+% - loss: type of loss, can be either 'hinge' for SVM or 'epsilon' for SVR
+%
+% OUTPUT:
+% - u: the optimal values for the coefficients of the linear
+%           combination of support vectors
+% - sv: the indices in X of the support vectors
+% - J: the function minimum value
+%
+% SEE ALSO quadprog, bundleizator
+
+%% Initialization
+m = size(X, 1);
+
+% QP solver options
+quadprog_options = optimoptions(@quadprog, 'Display', 'iter');
+
+% Get the SVs, and compute Gram matrices
+G = gram_matrix(X, kernel);
+sv = select_span_vectors(G);
+
+GX = G(:,sv);
+G = G(sv,sv);
+
+n = length(sv);
+
+%% Setup constraints
+if strcmpi(loss, 'hinge')
+    % Hinge loss for SVM
+    A = [zeros(m,n), -eye(m); bsxfun(@times, GX, y), -eye(m)];
+    b = [zeros(m,1); -ones(m,1)];
+else
+    % Epsilon-insensitive loss for SVR
+    eps = varargin{1};
+    A = [zeros(m,n), -eye(m); GX, -eye(m); -GX, -eye(m)];
+    b = [zeros(m,1); y + eps; -y + eps];
+end
+
+%% Solve QP
+H = [G * 2 / C, zeros(n,m); zeros(m,n+m)];
+g = [zeros(n,1); ones(m,1) / m];
+
+[x, J] = quadprog(H, g, A, b, [], [], [], [], [], quadprog_options);
+u = x(1:n);
+
+end
